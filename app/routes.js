ff609b40c05439ae140c5a09cc215b452fdb3d24
Liam Daniel <liam.daniel@engineering.digital.dwp.gov.uk>
2026-01-08 10:57:57 +0000

    V2 changes from feedack
    GitOrigin-RevId: 04ff2fabc7a1d7ff95c1a5d45023aa2c0bf4597b

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,17 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.post('/queue-type', function (req, res) {
+
+  var queueType = req.session.data['what-queue']
+
+  // Check whether the variable matches a condition
+  if (queueType== "home-office"){
+    // Send user to next page
+    res.redirect('/case-details')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/404')
+  }
+
+})
